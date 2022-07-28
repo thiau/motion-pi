367983b8f3ae6283e80616789a4c789a9e4805d7
Thiago Araujo <thiau.augusto@gmail.com>
2022-07-27 22:38:05 -0300

    Delete ifttt file

--- helpers/ifttt.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-
-def trigger_webhook(event_name: str, api_key: str):
-    response = requests.get(f"https://maker.ifttt.com/trigger/{event_name}/json/with/key/{api_key}")
-    return response
