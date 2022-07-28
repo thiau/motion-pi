541cbdb28cd5ee5e6e371c52587267d32d17da2c
Thiago Araujo <thiau.augusto@gmail.com>
2022-07-27 22:35:48 -0300

    Update server file

--- server.py
+++ server.py
@@ -1,23 +1,16 @@
-import time
 import logging
 from signal import pause
 from gpiozero import MotionSensor
 from dotenv import load_dotenv
-from helpers.ifttt import trigger_webhook
+from helpers.motion import MotionSense
 
 load_dotenv()
+
+motion_sense = MotionSense()
+
 logging.basicConfig(level=logging.INFO, format='%(levelname)s - %(asctime)s - %(message)s')
 logging.getLogger('server.py')
 
-lights_on = False
-
-def motion_detected():
-    # if not lights_on:
-    #     logging.info("Turn the lights on")
-    #     lights_on = True
-    logging.info("Motion")
-    # trigger_webhook("office_lights_on", "ciK5YTnqRD0Coe_ATf_zCd")
-
 pir = MotionSensor(18)
-pir.when_motion = motion_detected
+pir.when_motion = motion_sense.motion_detected
 pause()
